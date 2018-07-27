e6022ff1a136102f7545c4355aae4a7525fce80a
Gullitan <1994slavaslava@gmail.com>
2018-07-27 14:01:55 +0300

    ##

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-jQuery.fn.exists = function () {
-    return $(this).length;
+jQuery.fn.exists = function() {
+   return $(this).length;
 };
 
-jQuery(document).ready(function ($) {
-
+jQuery(document).ready(function($) {
+    $('.owl-carousel').owlCarousel({
+        items:5,
+        loop:true,
+        margin:10,
+        merge:true,
+        responsive:{
+            678:{
+                mergeFit:true
+            },
+            1000:{
+                mergeFit:false
+            }
+        }
+    });
 });
